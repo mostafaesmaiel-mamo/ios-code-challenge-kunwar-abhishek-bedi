7ad25feb4437c44b654a53000abf753bb644441c
Abhishek Bedi <kbedi@mafcarrefour.com>
2021-06-09 13:16:26 +0400

    Added Section Headers in CollectionView

--- ContactsApp/ChooseContactViewController.swift
+++ ContactsApp/ChooseContactViewController.swift
@@ -7,20 +7,83 @@
 
 import UIKit
 
+enum Section {
+    case frequentContacts
+    case mamoContacts
+    case phoneContacts
+    
+    var headerTitle: String {
+        switch self {
+        case .frequentContacts: return "Frequents"
+        case .mamoContacts: return "Your Friends On Mamo"
+        case .phoneContacts: return "Your Contacts"
+        }
+    }
+}
+
+
 class ChooseContactViewController: UICollectionViewController {
     fileprivate let cellId = "cellId"
+    fileprivate let headerId = "HeaderView"
     fileprivate let contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 16)
+    
+    var sections: [Section] = []
 
+    //MARK:- View Controller Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        sections.append(.frequentContacts)
+        sections.append(.mamoContacts)
+        sections.append(.phoneContacts)
 
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
-        collectionView.collectionViewLayout = createLayout()
+        setupCollectionView()
+    }
+}
+
+//MARK:- UICollectionViewCallbacks
+extension ChooseContactViewController {
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        sections.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = indexPath.section == 0 ? .systemRed : .systemBlue
+        
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerId, for: indexPath) as! HeaderView
+        let title = sections[indexPath.section].headerTitle
+        header.configure(withTitle: title)
+        return header
     }
 
+}
+
+
+
+private extension ChooseContactViewController {
+    
+    func setupCollectionView() {
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        
+        //TODO: Remove Hardcoding
+        collectionView.register(UINib.init(nibName: "HeaderView", bundle: .main), forSupplementaryViewOfKind: headerId, withReuseIdentifier: headerId)
+
+        collectionView.collectionViewLayout = createLayout()
+    }
+    
     func createLayout() -> UICollectionViewCompositionalLayout {
         return UICollectionViewCompositionalLayout { (section, env) -> NSCollectionLayoutSection? in
-
+            
             if section == 0 {
                 let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(0.25), heightDimension: .absolute(107)))
                 item.contentInsets.trailing = 22
@@ -32,6 +95,10 @@ class ChooseContactViewController: UICollectionViewController {
 
                 section.orthogonalScrollingBehavior = .continuous
                 
+                section.boundarySupplementaryItems = [
+                    .init(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .absolute(50)), elementKind: self.headerId, alignment: .topLeading)
+                ]
+                
                 return section
             } else {
                 let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .absolute(80)))
@@ -40,25 +107,11 @@ class ChooseContactViewController: UICollectionViewController {
                 let group = NSCollectionLayoutGroup.vertical(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .estimated(1000)), subitems: [item])
                 let section = NSCollectionLayoutSection(group: group)
                 section.contentInsets = self.contentInsets
+                section.boundarySupplementaryItems = [
+                    .init(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .absolute(50)), elementKind: self.headerId, alignment: .topLeading)
+                ]
                 return section
             }
         }
-    }
-
-    
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        2
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        10
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
-        
-        cell.backgroundColor = indexPath.section == 0 ? .systemRed : .systemBlue
-        
-        return cell
     }
 }

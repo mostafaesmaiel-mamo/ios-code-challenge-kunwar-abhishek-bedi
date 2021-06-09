e1707c7d98ce9791fe21b8a0ee19284e838907dd
Abhishek Bedi <kbedi@mafcarrefour.com>
2021-06-09 11:58:24 +0400

    WIP: Created Dummy UI with UICollectionViewCompositionalLayout

--- ContactsApp/ChooseContactViewController.swift
+++ ContactsApp/ChooseContactViewController.swift
@@ -7,14 +7,58 @@
 
 import UIKit
 
-class ChooseContactViewController: UIViewController {
+class ChooseContactViewController: UICollectionViewController {
+    fileprivate let cellId = "cellId"
+    fileprivate let contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 16)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        view.backgroundColor = .systemYellow
+
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView.collectionViewLayout = createLayout()
     }
 
+    func createLayout() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout { (section, env) -> NSCollectionLayoutSection? in
 
+            if section == 0 {
+                let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(0.25), heightDimension: .absolute(107)))
+                item.contentInsets.trailing = 22
+                item.contentInsets.bottom = 16
+                
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .absolute(150)), subitems: [item])
+                let section = NSCollectionLayoutSection(group: group)
+                section.contentInsets = self.contentInsets
+
+                section.orthogonalScrollingBehavior = .continuous
+                
+                return section
+            } else {
+                let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .absolute(80)))
+                item.contentInsets.bottom = 16
+
+                let group = NSCollectionLayoutGroup.vertical(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .estimated(1000)), subitems: [item])
+                let section = NSCollectionLayoutSection(group: group)
+                section.contentInsets = self.contentInsets
+                return section
+            }
+        }
+    }
+
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        2
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        
+        cell.backgroundColor = indexPath.section == 0 ? .systemRed : .systemBlue
+        
+        return cell
+    }
 }
-

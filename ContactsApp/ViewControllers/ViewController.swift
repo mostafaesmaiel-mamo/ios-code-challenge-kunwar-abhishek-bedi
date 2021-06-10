b838db98730e5102f2ed3ac7f3577d712c709cbe
Abhishek Bedi <kbedi@mafcarrefour.com>
2021-06-10 14:13:57 +0400

    Added Button Actions for View Controller

--- ContactsApp/ViewControllers/ViewController.swift
+++ ContactsApp/ViewControllers/ViewController.swift
@@ -9,13 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //:MARK - View Controller Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        //  Do any additional setup after loading the view.
     }
     
-
+    //:MARK - IBActions
+    @IBAction func onEnglishButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func onArabicButtonPressed(_ sender: Any) {
+    }
     /*
     // MARK: - Navigation
 

7fe10e24c3fee8782abfc2e0188229c73146c3b1
Abhishek Bedi <kbedi@mafcarrefour.com>
2021-06-10 14:46:41 +0400

    Added User Defaults Utility class for handling persistent data

---
+++ ContactsApp/Utils/UserDefaultsUtility.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaultsUtility.swift
+//  ContactsApp
+//
+//  Created by kbedi on 10/06/2021.
+//
+
+import Foundation
+import SwiftyUserDefaults
+
+enum UserDefaultKey: StringLiteralType {
+    case didUserGaveContactsPermission
+}
+
+extension DefaultsKeys {
+    var didUserGaveContactsPermission: DefaultsKey<Bool?> { .init(UserDefaultKey.didUserGaveContactsPermission.rawValue, defaultValue: false) }
+}

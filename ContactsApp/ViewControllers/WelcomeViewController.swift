8f4d638472058a872d1543d3713dc1146e66841d
Abhishek Bedi <kbedi@mafcarrefour.com>
2021-06-10 14:47:55 +0400

    Renamed ViewController to WelcomeViewController

--- ContactsApp/ViewControllers/WelcomeViewController.swift
+++ ContactsApp/ViewControllers/WelcomeViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeViewController: UIViewController {
 
     //:MARK - View Controller Lifecycle Methods
     override func viewDidLoad() {
@@ -16,20 +16,13 @@ class ViewController: UIViewController {
         //  Do any additional setup after loading the view.
     }
     
-    //:MARK - IBActions
+    
+    //MARK: - IBActions
     @IBAction func onEnglishButtonPressed(_ sender: Any) {
     }
     
     @IBAction func onArabicButtonPressed(_ sender: Any) {
+    
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }

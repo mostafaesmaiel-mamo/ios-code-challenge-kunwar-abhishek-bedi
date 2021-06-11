4790f0c6a98a79fe57bc5cdeaf72652a607a9d07
Abhishek Bedi <kbedi@mafcarrefour.com>
2021-06-11 18:54:23 +0400

    Added //Mark

--- ContactsApp/ViewControllers/ChooseRecipientViewController.swift
+++ ContactsApp/ViewControllers/ChooseRecipientViewController.swift
@@ -9,7 +9,10 @@ import UIKit
 
 class ChooseRecipientViewController: UIViewController {
     
+	//MARK: - IBOutlets
     @IBOutlet weak var containerView: UIView!
+	
+	//MARK: - ViewController Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "#Choose Recipient"

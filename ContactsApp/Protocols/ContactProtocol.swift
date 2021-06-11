8d3d063dfced1b38be2f98aaa334971e01dc8d95
Abhishek Bedi <kbedi@mafcarrefour.com>
2021-06-12 00:13:47 +0400

    Added Loader and Contact Manager

--- ContactsApp/Protocols/ContactProtocol.swift
+++ ContactsApp/Protocols/ContactProtocol.swift
@@ -4,7 +4,17 @@
 //
 //  Created by kbedi on 09/06/2021.
 //
+import ContactsUI
 
+
+/// To fetch contacts from Phone
+protocol PhoneContactsFetchable {
+	var store: CNContactStore {get set}
+	var keys: [String] {get set}
+	func fetchContacts(onCompletion: @escaping (Bool) -> ())
+}
+
+/// To present the contact on UI
 protocol ContactPresentable {
     func configure(withFirstName firstName: String, lastName: String, imageUrl: String?)
 }
--- ContactsApp/ViewControllers/ChooseRecipientViewController.swift
+++ ContactsApp/ViewControllers/ChooseRecipientViewController.swift
@@ -10,11 +10,55 @@ import UIKit
 class ChooseRecipientViewController: UIViewController {
     
 	//MARK: - IBOutlets
-    @IBOutlet weak var containerView: UIView!
+	@IBOutlet weak var containerView: UIView!
+	@IBOutlet weak var footerView: UIView!
+
+	//MARK: - Variables
+	private var contactManager: PhoneContactsFetchable!
+	
+	lazy var loader = LoaderView()
+	
+	func configure(withContactManager manager: PhoneContactsFetchable = ContactsManager()) {
+		contactManager = manager
+	}
 	
 	//MARK: - ViewController Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "#Choose Recipient"
+		
+		guard contactManager != nil else {
+			fatalError("Contact Manager Cannot be Nil")
+		}
+		
+		fetchPhoneContacts()
     }
+}
+
+fileprivate extension ChooseRecipientViewController {
+	
+	func fetchPhoneContacts() {
+		showHideViewsWhileLoading(canShow: false)
+		loader.showOn(self.view)
+		
+//		contactManager.fetchContacts(fromStore: <#T##CNContactStore#>, withKeys: <#T##[String]#>, onCompletion: <#T##(Bool) -> ()#>)
+		
+		contactManager.fetchContacts() { [weak self] isSuccess in
+			DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+				self?.showHideViewsWhileLoading(canShow: true)
+				self?.loader.hide()
+			}
+		}
+
+	}
+	func showHideViewsWhileLoading(canShow: Bool) {
+		if canShow {
+			containerView.isHidden = false
+			footerView.isHidden = false
+		}
+		else {
+			containerView.isHidden = true
+			footerView.isHidden = true
+		}
+	}
 }

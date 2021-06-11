9e5f05b0fd7675a5bfb16c7bc3f39a8e58bc8a56
Abhishek Bedi <kbedi@mafcarrefour.com>
2021-06-12 00:44:35 +0400

    Cleanup ChooseRecipientViewController

--- ContactsApp/ViewControllers/ChooseRecipientViewController.swift
+++ ContactsApp/ViewControllers/ChooseRecipientViewController.swift
@@ -15,12 +15,7 @@ class ChooseRecipientViewController: UIViewController {
 
 	//MARK: - Variables
 	private var contactManager: PhoneContactsFetchable!
-	
 	lazy var loader = LoaderView()
-	
-	func configure(withContactManager manager: PhoneContactsFetchable = ContactsManager()) {
-		contactManager = manager
-	}
 	
 	//MARK: - ViewController Lifecycle Methods
     override func viewDidLoad() {
@@ -32,7 +27,12 @@ class ChooseRecipientViewController: UIViewController {
 		}
 		
 		fetchPhoneContacts()
-    }
+	}
+	
+	//MARK: - Configuration Methods
+	func configure(withContactManager manager: PhoneContactsFetchable = ContactsManager()) {
+		contactManager = manager
+	}
 }
 
 fileprivate extension ChooseRecipientViewController {
@@ -41,16 +41,14 @@ fileprivate extension ChooseRecipientViewController {
 		showHideViewsWhileLoading(canShow: false)
 		loader.showOn(self.view)
 		
-//		contactManager.fetchContacts(fromStore: <#T##CNContactStore#>, withKeys: <#T##[String]#>, onCompletion: <#T##(Bool) -> ()#>)
-		
 		contactManager.fetchContacts() { [weak self] isSuccess in
 			DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
 				self?.showHideViewsWhileLoading(canShow: true)
 				self?.loader.hide()
 			}
 		}
-
 	}
+		
 	func showHideViewsWhileLoading(canShow: Bool) {
 		if canShow {
 			containerView.isHidden = false
